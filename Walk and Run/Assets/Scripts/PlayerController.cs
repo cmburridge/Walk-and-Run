46982a9e899cfa93f3a6f27032b0d2995fc96383
Cameron burridge <cmburridge4022@gmail.com>
2020-09-30 17:10:52 -0600

    Created a walk and run script using rigidbodies

---
+++ Walk and Run/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 30f;
+    public float fastSpeed = 40f;
+
+    private Rigidbody rBody;
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var horAxis = Input.GetAxis("Horizontal");
+        var vertAxis = Input.GetAxis("Vertical");
+        
+        var movement = Time.fixedDeltaTime * moveSpeed * new Vector3(horAxis, 0, vertAxis);
+        
+        rBody.MovePosition(transform.position + movement);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = fastSpeed;
+        }
+        else
+        {
+            moveSpeed = 15f;
+        }
+    }
+}
---
+++ Walk and Run/Assets/Spin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float spinSpeed = 1f;
+    void Update()
+    {
+        transform.Rotate(0,spinSpeed,0);
+    }
+}
